479c9fb2696ed5a6ec6156c4244d69dbf762b27b
Max Gordon <max.gordon@snhu.edu>
2021-09-24 13:27:12 -0400

    work through chapter 4 up until apply tests code

--- messageBoard/settings.py
+++ messageBoard/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'messageBoard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [str(BASE_DIR.joinpath('templates'))]
         ,
         'APP_DIRS': True,
         'OPTIONS': {

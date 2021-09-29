1172036b10ca3216431e91b7e600169fa75d3869
Max Gordon <max.gordon@snhu.edu>
2021-09-29 13:24:13 -0400

    add gunicorn, procfile, heroku

--- messageBoard/settings.py
+++ messageBoard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-puf0jo%@a5ze(8^u-)f&0&d*j@)s1saf_y0=!n-=(ta2i0^%_-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
